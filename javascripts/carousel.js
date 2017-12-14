5e6d5831382c85ed12d8614eb14a260dd0443629
edwinchen85 <edwinchen85@gmail.com>
2017-12-15 05:59:13 +0800

    Set up references and  skeletal structure of functions

--- javascripts/carousel.js
+++ javascripts/carousel.js
@@ -1,4 +1,52 @@
 $(function() {
+  var prevIndex = 0;
+  var currentIndex = 1;
+  var nextIndex = 2;
+  var lastIndex = $('#quotes-carousel').find('.quote').length - 1;
 
+  $('#quotes-carousel').on('click', '.previous', showQuote);
+  $('#quotes-carousel').on('click', '.next', showQuote);
+  $('#quotes-carousel-pips').on('click', '.pip', showFromPip);
+
+  generatePips();
+
+  var carouselRunning = true;
+  var interval = setInterval(function() {
+    if (carouselRunning) {
+      showNextQuote();
+    }
+  }, 4000);
+
+  function showNextQuote() {
+
+  }
+
+  function showQuote() {
+
+  }
+
+  function updateState(index) {
+
+  }
+
+  function updateCarouselPosition() {
+
+  }
+
+  function generatePips() {
+
+  }
+
+  function updatePips() {
+
+  }
+
+  function showFromPip() {
+
+  }
+
+  function setLeftClass() {
+
+  }
 });
 
